36f04199e32da152b0a176a8322a7ebcbf00c32a
Beni Stocker <benjamin.stocker@gmail.com>
2024-05-07 15:23:40 +0200

    added potential ET vignette

--- data-raw/get_data.R
+++ data-raw/get_data.R
@@ -3,9 +3,16 @@ library(dplyr)
 library(here)
 library(lubridate)
 
-# get fluxnet data for FR-Pue site
+# get fluxnet data for CH-Lae site
 df <- read_csv(paste0(here(), "/inst/extdata/FLX_CH-Lae_FLUXDATAKIT_FULLSET_DD_2004_2014_2-3.csv")) |>
-  select(TIMESTAMP, P_F, TA_F_MDS, PA_F, LE_F_MDS) |>
+  select(TIMESTAMP, P_F, TA_F_MDS, PA_F, LE_F_MDS, NETRAD) |>
   filter(year(TIMESTAMP) > 2004)
 
 saveRDS(df, file = paste0(here(), "/data/df_ch-lae.rds"))
+
+
+# get fluxnet data for FR-Pue site
+df <- read_csv(paste0(here(), "/inst/extdata/FLX_FR-Pue_FLUXDATAKIT_FULLSET_DD_2000_2014_2-3.csv")) |>
+  select(TIMESTAMP, P_F, TA_F_MDS, PA_F, LE_F_MDS, NETRAD)
+
+saveRDS(df, file = paste0(here(), "/data/df_fr-pue.rds"))
